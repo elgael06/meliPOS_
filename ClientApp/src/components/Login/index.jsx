96e3192a63d6cfd26692132d270c869f98dcf702
cgvalenzuelac <cgvalenzuelac@concredito.com.mx>
2019-11-05 16:14:00 -0700

    adding styles and login.

--- ClientApp/src/components/Login/index.jsx
+++ ClientApp/src/components/Login/index.jsx
@@ -23,19 +23,50 @@ const Login =({actions})=>{
     <Redirect to="/login"/> 
     <Switch>
         <Route path="/login" >
-            <div className="login-container">
-                <label >this is the <b>login</b></label>
-                <hr/>
-                <form onSubmit={click}>
-                    <div><input type="text" value={id} onChange={e=>setId(e.target.value)} placeholder="ID" /></div>
-                    <div><input type="password" value={password} onChange={e=>setPasswd(e.target.value)} placeholder="password" /></div>
-                    <button type="submit" onClick={click}>Login</button>
-                </form>
+            <div className="login-container authentication-theme auth-style_1">
+                <CuerpoLogin>
+                    <div className="row">
+                        <div className="col-12 logo-section center" >
+                            <h1 className="logo">login MeliPOS</h1>
+                        </div>
+                    </div>
+                    <form onSubmit={click} className="col-lg-7 col-md-8 col-sm-9 col-12 mx-auto form-wrapper">
+                        <div className="form-group input-rounded">
+                            <input 
+                                type="text" 
+                                className="form-control" 
+                                value={id} 
+                                onChange={e=>setId(e.target.value)} 
+                                placeholder="ID" 
+                            />
+                        </div>
+                        <div  className="form-group input-rounded">
+                            <input
+                                type="password" 
+                                className="form-control" 
+                                value={password} 
+                                onChange={e=>setPasswd(e.target.value)} 
+                                placeholder="password"
+                            />
+                        </div>
+                        <button type="submit" className="btn btn-primary btn-block" onClick={click}>Login</button>
+                    </form>
+                </CuerpoLogin>
             </div>
         </Route>
     </Switch>
     </Fragment>);
 }
+
+const CuerpoLogin=({children})=>(<div className="row">
+     <div className="col-lg-5 col-md-7 col-sm-9 col-11 mx-auto">
+        <div className="grid">
+        <div className="grid-body">
+            {children}
+        </div>
+        </div>
+     </div>
+</div>);
 
 const mapStateProps=()=>({
 
